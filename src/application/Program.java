bd736215b1886f0667fd484f16e2385e30f5ebdd
Matheus Rafael <matheusrafael2005@gmail.com>
2025-06-05 15:55:22 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -12,19 +13,24 @@ public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(1);
-		System.out.println("=========== Test 1 - FindByID ========");
+		System.out.println("=========== Test 1 - seller FindByID ========");
 		System.out.println(seller);
-		System.out.println("=========== Test 2 - FindByDepartment ========");
+		System.out.println("=========== Test 2 - seller FindByDepartment ========");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
-		System.out.println("=========== Test 3 - FindAll ========");
+		System.out.println("=========== Test 3 - seller FindAll ========");
 		List<Seller> list1 = sellerDao.findAll();
 		for(Seller obj: list1) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=========== Test 4 - seller Insert ========");
+		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +26,35 @@ public class SellerDaoJDBC implements SellerDao{
 	
 	@Override
 	public void insert(Seller d) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?) ",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, d.getName());
+			st.setString(2, d.getEmail());
+			st.setDate(3, new java.sql.Date(d.getBirthDate().getTime()));
+			st.setDouble(4, d.getBaseSalary());
+			st.setInt(5, d.getDepartment().getId());
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected>0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					d.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
+		
 		
 	}
 
